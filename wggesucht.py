542b7ae4c6fe553c7176931349b3d1b1153dfa73
christianheins <christian_heins@hotmail.com>
2023-04-09 11:45:44 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -704,12 +704,15 @@ def main():
             source = pd.DataFrame({"Category": ["Indefinite term", "Limited term"], "Value": [df_concat_pivot_longterm, df_concat_pivot_shortterm]})
             st.markdown("<h6 style='text-align: center; color: orange;'>Lease terms</h6>", unsafe_allow_html=True)
 
-            chart = alt.Chart(source).mark_arc(innerRadius=90, legend=alt.Legend(orient='left')).encode(
+            chart = alt.Chart(source).mark_arc(innerRadius=90).encode(
                 theta='Value:Q',
                 color=alt.Color('Category', scale=alt.Scale(scheme='category10')),
                 tooltip=['Value:Q'],
+                legend=alt.Legend(orient='left')
             )
-
+            chart = chart.configure_legend(
+                orient='left'
+            )
             st.altair_chart(chart.interactive(), use_container_width=True)
 
         with col2:
