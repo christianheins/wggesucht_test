529f6747b1f93160d126a03fcd6a2087e4af2ee4
christianheins <christian_heins@hotmail.com>
2023-04-14 11:55:14 +0200

    Testing the commit command

--- wggesucht.py
+++ wggesucht.py
@@ -718,7 +718,7 @@ def main():
         # convert timestamp into DateTime object
         dt_m = dt.datetime.fromtimestamp(m_time).strftime("%d/%m/%Y - %H:%M:%S")
         st.write(f'File last created on: {dt_m}')
-    st.markdown("<a href='https://airtable.com/app5p6oAnyclgWPM9/pagFlz6Ae0kMjHNNu?RwACb=rec5EKs8ZMeOsygjH&vH2xA=reczG0JNSvJCezJDx'>Click here to go the disposals dashboard</a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://www.linkedin.com/in/christian-h-0545aaa1/'>Find me on LinkedIn</a>", unsafe_allow_html=True)
 
     df_concat = pd.read_csv(nameofdataframe)
 
